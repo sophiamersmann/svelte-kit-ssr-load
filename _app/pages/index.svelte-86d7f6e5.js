bb6c92517f1aad22a30538ccc9f2378eeafbac22
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-12-10 16:50:41 +0100

    Updates

--- _app/pages/index.svelte-86d7f6e5.js
+++ _app/pages/index.svelte-86d7f6e5.js
@@ -6,4 +6,4 @@ that loads data from /api/data.json`),this.h()},l(t){s=v(t,"H1",{});var a=_(s);k
 
 I am the index page and link to a
 `),i=v(t,"A",{href:!0});var A=_(i);S=l(A,"page"),A.forEach(e),m=l(t,`
-that loads data from /api/data.json`),this.h()},h(){H(f,"href","https://kit.svelte.dev"),H(i,"href","/"+K+"/PageLoadingData")},m(t,a){r(t,s,a),d(s,k),r(t,p,a),r(t,o,a),d(o,x),d(o,f),d(f,j),d(o,E),r(t,h,a),r(t,i,a),d(i,S),r(t,m,a)},p:b,i:b,o:b,d(t){t&&e(s),t&&e(p),t&&e(o),t&&e(h),t&&e(i),t&&e(m)}}}function D(I){return console.log(K),[]}class w extends P{constructor(s){super();V(this,s,D,C,W,{})}}export{w as default};
+that loads data from /api/data.json`),this.h()},h(){H(f,"href","https://kit.svelte.dev"),H(i,"href",""+(K+"/PageLoadingData"))},m(t,a){r(t,s,a),d(s,k),r(t,p,a),r(t,o,a),d(o,x),d(o,f),d(f,j),d(o,E),r(t,h,a),r(t,i,a),d(i,S),r(t,m,a)},p:b,i:b,o:b,d(t){t&&e(s),t&&e(p),t&&e(o),t&&e(h),t&&e(i),t&&e(m)}}}function D(I){return console.log(K),[]}class w extends P{constructor(s){super();V(this,s,D,C,W,{})}}export{w as default};
