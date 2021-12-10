d1943907fcee6b2e338dc4e434902b933f803e77
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-12-10 16:32:56 +0100

    Updates

---
+++ _app/pages/index.svelte-45fda8e5.js
@@ -0,0 +1,9 @@
+import{S as W,i as $,s as g,e as c,t as l,k as q,c as d,a as m,g as n,d as t,n as C,b as j,f as r,H as p,I as y}from"../chunks/vendor-af0f2ef7.js";import{b as D}from"../chunks/paths-28a87002.js";function P(H){let s,x,u,i,S,h,E,b,v,o,A,_,f,I;return{c(){s=c("h1"),x=l("Welcome to SvelteKit"),u=q(),i=c("p"),S=l("Visit "),h=c("a"),E=l("kit.svelte.dev"),b=l(" to read the documentation"),v=l(`
+
+I am the index page and link to
+`),o=c("a"),A=l("page 1"),_=l(`
+and `),f=c("a"),I=l("page 2"),this.h()},l(e){s=d(e,"H1",{});var a=m(s);x=n(a,"Welcome to SvelteKit"),a.forEach(t),u=C(e),i=d(e,"P",{});var k=m(i);S=n(k,"Visit "),h=d(k,"A",{href:!0});var K=m(h);E=n(K,"kit.svelte.dev"),K.forEach(t),b=n(k," to read the documentation"),k.forEach(t),v=n(e,`
+
+I am the index page and link to
+`),o=d(e,"A",{href:!0});var L=m(o);A=n(L,"page 1"),L.forEach(t),_=n(e,`
+and `),f=d(e,"A",{href:!0});var V=m(f);I=n(V,"page 2"),V.forEach(t),this.h()},h(){j(h,"href","https://kit.svelte.dev"),j(o,"href",""+(D+"/LoadingDataUsingSlug")),j(f,"href",""+(D+"/LoadingStaticData"))},m(e,a){r(e,s,a),p(s,x),r(e,u,a),r(e,i,a),p(i,S),p(i,h),p(h,E),p(i,b),r(e,v,a),r(e,o,a),p(o,A),r(e,_,a),r(e,f,a),p(f,I)},p:y,i:y,o:y,d(e){e&&t(s),e&&t(u),e&&t(i),e&&t(v),e&&t(o),e&&t(_),e&&t(f)}}}function R(H){return console.log(D),[]}class z extends W{constructor(s){super();$(this,s,R,P,g,{})}}export{z as default};
