4a09067a29b17b23d2d9f4fd77496f793981b724
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-12-10 16:18:59 +0100

    Updates

--- _app/pages/index.svelte-1aae0b95.js
+++
@@ -1,7 +0,0 @@
-import{S as P,i as V,s as W,e as m,t as o,k as b,c as v,a as u,g as r,d as t,n as j,b as H,f as h,H as f,I}from"../chunks/vendor-af0f2ef7.js";function q(K){let a,_,p,s,k,n,x,E,d,i,S;return{c(){a=m("h1"),_=o("Welcome to SvelteKit"),p=b(),s=m("p"),k=o("Visit "),n=m("a"),x=o("kit.svelte.dev"),E=o(" to read the documentation"),d=o(`
-
-I am the index page and link to
-`),i=m("a"),S=o("page 1"),this.h()},l(e){a=v(e,"H1",{});var l=u(a);_=r(l,"Welcome to SvelteKit"),l.forEach(t),p=j(e),s=v(e,"P",{});var c=u(s);k=r(c,"Visit "),n=v(c,"A",{href:!0});var y=u(n);x=r(y,"kit.svelte.dev"),y.forEach(t),E=r(c," to read the documentation"),c.forEach(t),d=r(e,`
-
-I am the index page and link to
-`),i=v(e,"A",{href:!0});var A=u(i);S=r(A,"page 1"),A.forEach(t),this.h()},h(){H(n,"href","https://kit.svelte.dev"),H(i,"href","/Page1")},m(e,l){h(e,a,l),f(a,_),h(e,p,l),h(e,s,l),f(s,k),f(s,n),f(n,x),f(s,E),h(e,d,l),h(e,i,l),f(i,S)},p:I,i:I,o:I,d(e){e&&t(a),e&&t(p),e&&t(s),e&&t(d),e&&t(i)}}}class R extends P{constructor(a){super();V(this,a,null,q,W,{})}}export{R as default};
