be081acbb9a0db8556c9543892f993596213b42f
Sophia Mersmann <sophia.mersmann1@gmail.com>
2022-01-14 11:01:22 +0100

    Updates

---
+++ _app/pages/index.svelte-1705a8e4.js
@@ -0,0 +1,5 @@
+import{S as p,i as m,s as c,t as l,e as u,g as r,c as _,a as x,d as i,b as g,f as d,H as j,I as f}from"../chunks/vendor-af0f2ef7.js";function k(h){let e,t,o,n;return{c(){e=l(`I am the index page and link to a
+`),t=u("a"),o=l("page"),n=l(`
+that loads data from /api/data.json`),this.h()},l(a){e=r(a,`I am the index page and link to a
+`),t=_(a,"A",{href:!0});var s=x(t);o=r(s,"page"),s.forEach(i),n=r(a,`
+that loads data from /api/data.json`),this.h()},h(){g(t,"href","PageLoadingData")},m(a,s){d(a,e,s),d(a,t,s),j(t,o),d(a,n,s)},p:f,i:f,o:f,d(a){a&&i(e),a&&i(t),a&&i(n)}}}class I extends p{constructor(e){super();m(this,e,null,k,c,{})}}export{I as default};
--- _app/pages/index.svelte-e4b4fc9c.js
+++
@@ -1,9 +0,0 @@
-import{S as P,i as V,s as W,e as u,t as i,k as $,c as v,a as _,g as l,d as e,n as q,b as H,f,H as d,I as b}from"../chunks/vendor-af0f2ef7.js";import{b as K}from"../chunks/paths-28a87002.js";function C(I){let s,k,p,o,x,r,j,E,h,n,S,m;return{c(){s=u("h1"),k=i("Welcome to SvelteKit"),p=$(),o=u("p"),x=i("Visit "),r=u("a"),j=i("kit.svelte.dev"),E=i(" to read the documentation"),h=i(`
-
-I am the index page and link to a
-`),n=u("a"),S=i("page"),m=i(`
-that loads data from /api/data.json`),this.h()},l(t){s=v(t,"H1",{});var a=_(s);k=l(a,"Welcome to SvelteKit"),a.forEach(e),p=q(t),o=v(t,"P",{});var c=_(o);x=l(c,"Visit "),r=v(c,"A",{href:!0});var y=_(r);j=l(y,"kit.svelte.dev"),y.forEach(e),E=l(c," to read the documentation"),c.forEach(e),h=l(t,`
-
-I am the index page and link to a
-`),n=v(t,"A",{href:!0});var A=_(n);S=l(A,"page"),A.forEach(e),m=l(t,`
-that loads data from /api/data.json`),this.h()},h(){H(r,"href","https://kit.svelte.dev"),H(n,"href",""+(K+"/PageLoadingData"))},m(t,a){f(t,s,a),d(s,k),f(t,p,a),f(t,o,a),d(o,x),d(o,r),d(r,j),d(o,E),f(t,h,a),f(t,n,a),d(n,S),f(t,m,a)},p:b,i:b,o:b,d(t){t&&e(s),t&&e(p),t&&e(o),t&&e(h),t&&e(n),t&&e(m)}}}const w=!1;function D(I){return console.log(K),[]}class z extends P{constructor(s){super();V(this,s,D,C,W,{})}}export{z as default,w as router};
