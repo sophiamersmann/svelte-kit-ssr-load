83595cb53d547f4ded3d1c5bf0a0424a7e8ee34a
Sophia Mersmann <sophia.mersmann1@gmail.com>
2022-01-14 11:18:20 +0100

    Updates

---
+++ _app/pages/index.svelte-747463ae.js
@@ -0,0 +1,5 @@
+import{S as j,i as v,s as y,t as i,e as _,g as l,c as x,a as b,d as n,J as g,b as I,f as r,H as k,I as u}from"../chunks/vendor-085b0623.js";import{b as S}from"../chunks/paths-28a87002.js";function A(c){let o,e,d,f,s,m,p;return{c(){o=i("I am the "),e=_("span"),d=i("index"),f=i(` page and link to a
+`),s=_("a"),m=i("page"),p=i(`
+that loads data from /api/data.json`),this.h()},l(a){o=l(a,"I am the "),e=x(a,"SPAN",{style:!0});var t=b(e);d=l(t,"index"),t.forEach(n),f=l(a,` page and link to a
+`),s=x(a,"A",{href:!0});var h=b(s);m=l(h,"page"),h.forEach(n),p=l(a,`
+that loads data from /api/data.json`),this.h()},h(){g(e,"background-color","orange"),I(s,"href","PageLoadingData")},m(a,t){r(a,o,t),r(a,e,t),k(e,d),r(a,f,t),r(a,s,t),k(s,m),r(a,p,t)},p:u,i:u,o:u,d(a){a&&n(o),a&&n(e),a&&n(f),a&&n(s),a&&n(p)}}}function E(c){return console.log("base in index",S),[]}class q extends j{constructor(o){super();v(this,o,E,A,y,{})}}export{q as default};
