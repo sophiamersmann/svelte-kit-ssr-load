3b0111bb7013e4fa2579c5e93a466553b3e06c38
Sophia Mersmann <sophia.mersmann1@gmail.com>
2022-01-14 11:14:28 +0100

    Updates

--- _app/pages/index.svelte-e6df7f7c.js
+++
@@ -1,5 +0,0 @@
-import{S as p,i as m,s as c,t as r,e as u,g as l,c as x,a as _,d as o,b as g,f,H as b,I as d}from"../chunks/vendor-af0f2ef7.js";import{b as j}from"../chunks/paths-28a87002.js";function k(h){let e,t,i,n;return{c(){e=r(`I am the index page and link to a
-`),t=u("a"),i=r("page"),n=r(`
-that loads data from /api/data.json`),this.h()},l(a){e=l(a,`I am the index page and link to a
-`),t=x(a,"A",{href:!0});var s=_(t);i=l(s,"page"),s.forEach(o),n=l(a,`
-that loads data from /api/data.json`),this.h()},h(){g(t,"href","PageLoadingData")},m(a,s){f(a,e,s),f(a,t,s),b(t,i),f(a,n,s)},p:d,i:d,o:d,d(a){a&&o(e),a&&o(t),a&&o(n)}}}function v(h){return console.log("base in index",j),[]}class S extends p{constructor(e){super();m(this,e,v,k,c,{})}}export{S as default};
